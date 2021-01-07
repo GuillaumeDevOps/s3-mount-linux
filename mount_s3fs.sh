1af44d9f74027129ae8bdc4703c1e17fc00a91ac
Guillaume Isabelle <jgi@jgwill.com>
2021-01-07 07:30:34 -0500

    fix: mount path

--- mount_s3fs.sh
+++ mount_s3fs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export s3bucket=audio-for-wordpress-43760614807f9d04391cb32d0d1651cfe4a2db0e
-export s3mountpath=:~/_s3fs/audio-for-wordpress-43760614807f9d04391cb32d0d1651cfe4a2db0e
+export s3mountpath=~/_s3fs/audio-for-wordpress-43760614807f9d04391cb32d0d1651cfe4a2db0e
 
 # Might not change often
 export s3_passwd_file=~/.passwd-s3fs
